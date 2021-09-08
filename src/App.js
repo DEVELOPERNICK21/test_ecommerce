6903cc96c2945691b2c2bf1eb9cfe6e64ad0593e
NICK KUBDE <nikhilkubde21@gmail.com>
2021-09-08 06:54:16 +0530

    checking the git ssh

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-// import Header from './components/style'
 import {
   BrowserRouter as Router,
   Switch,
   Route,
 } from "react-router-dom";
-import Menu from './components/Menu/menu'
-import Thank from './components/thankYou/thank'
 import LogIn from './components/LogIn/LogIn'
 import SignUp from './components/SignUp/SignUp'
 
@@ -15,15 +12,7 @@ function App() {
     <Router>
     <Switch>
       <Route exact path="/" component={LogIn} />
-      {/* <Route exact path="/" component={Header} /> */}
       <Route exact path="/signup" component={SignUp} />
-      <Route exact path="/menu" component={Menu} />
-      <Route exact path="/checkout" component={Thank} />
-    {/* <div className="App">      
-    <Header />
-    <Menu/>
-    <Thank/>    
-    </div> */}
     </Switch>
     </Router>
     </>
